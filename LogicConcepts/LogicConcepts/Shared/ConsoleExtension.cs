933d11d90cb085e7153b4d64a5574326b07e0bc0
Sandra Cardona Ocampo <sami-1780@hotmail.com>
2025-05-19 18:09:15 -0700

    Project created

---
+++ LogicConcepts/LogicConcepts/Shared/ConsoleExtension.cs
@@ -0,0 +1,35 @@
+﻿namespace Shared 
+{
+
+    public class ConsoleExtension
+    {
+        
+        public static int GetInt(string message) 
+        {
+            Console.Write(message);
+            var numberString = Console.ReadLine();
+            var numberInt = 0;
+            if (int.TryParse(numberString, out numberInt)) 
+            {
+                return numberInt;
+            }
+
+            return 0;
+        }
+    
+
+        public static string GetValidOptions(string message, List<string> options) 
+        {
+            Console.Write(message);
+            var answer = Console.ReadLine();
+            if (options.Any(x => x.Equals(answer, StringComparison.CurrentCultureIgnoreCase)))
+            {
+                return answer;
+            }
+            return null;
+        }
+
+    }
+   
+
+}
---
+++ LogicConcepts/README.md
@@ -0,0 +1,3 @@
+# LogicConcepts
+
+Este es un repo para repasar los conceptos de lógica, vamos a utilizar C# y VisualStudio
